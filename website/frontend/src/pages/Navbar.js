9332ddb58921fa33dbd1c8975b9689bdd396bf17
developer-ishan <ishan@mnnit.ac.in>
2021-10-03 14:49:27 +0530

    Navbar logo correction

--- website/frontend/src/pages/Navbar.js
+++ website/frontend/src/pages/Navbar.js
@@ -4,25 +4,33 @@ import NavItems from "./NavItems";
 import logo from "../assets/blaze.png";
 
 const Navbar = (props) => {
-    return (
-        <div className='nav-container'>
-            <div className='main-nav'>
-                <div className="row">
-                <div class="col-sm-3">
-                <div className='logo'>
-                    <a target={"_blank"} href='/'>
-                        <img src={logo} alt='Blaze' className='logo-img' />
-                    </a>
-                </div>
-                </div>
-                <div className="col-sm-3 logo-text">
-                    <span>Vision Voice</span>
-                </div>
-                </div>
-                <NavItems authenticated={props.authenticated} handleNotAuthenticated={props._handleNotAuthenticated} />
+  return (
+    <div className="nav-container">
+      <div className="main-nav">
+        {/* <div className="row">
+          <div class="col-sm-3">
+            <div className="logo">
+              <a target={"_blank"} href="/">
+                <img src={logo} alt="Blaze" className="logo-img" />
+              </a>
             </div>
+          </div>
+          <div className="col-sm-3 logo-text">
+            <span>Vision Voice</span>
+          </div>
+        </div> */}
+        <div>
+          <a target={"_blank"} href="/">
+            <img src={logo} alt="Blaze" className="logo-img" />
+          </a>
         </div>
-    );
+        <NavItems
+          authenticated={props.authenticated}
+          handleNotAuthenticated={props._handleNotAuthenticated}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
