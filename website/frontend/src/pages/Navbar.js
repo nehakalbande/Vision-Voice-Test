fb53fa5dc528ba51691c24917ec7185e063c0fe8
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2021-10-07 11:28:31 +0530

    make navbar brand responsive(#45)
    solved issue #45

--- website/frontend/src/pages/Navbar.js
+++ website/frontend/src/pages/Navbar.js
@@ -3,27 +3,27 @@ import "../css/Navbar.css";
 import NavItems from "./NavItems";
 import logo from "../assets/blaze.png";
 import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
 const Navbar = () => {
-    
-    return (
-        <div className='nav-container'>
-            <div className='main-nav'>
-                <div className="row">
-                <div className="col-sm-3">
-                <div className='logo'>
-                    <Link target="_blank" to='/'>
-                        <img src={logo} alt='Blaze' className='logo-img' />
-                    </Link>
-                </div>
-                </div>
-                <div className="col-sm-3 logo-text">
-                    <span>Vision Voice</span>
-                </div>
-                </div>
-                <NavItems/>
+  return (
+    <div className="nav-container">
+      <div className="main-nav">
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="logo">
+              <Link target="_blank" to="/">
+                <img src={logo} alt="Blaze" className="logo-img" />
+              </Link>
             </div>
+          </div>
+          <div className="col-sm-6 logo-text">
+            <span>Vision Voice</span>
+          </div>
         </div>
-    );
+        <NavItems />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
